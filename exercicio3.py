313cbc826c2b38de3b889d2bb62c168ffcba5fbe
Giovanna <102827629+GiovannaVianaCorreia@users.noreply.github.com>
2022-04-29 15:52:43 -0300

    Add files via upload

---
+++ exercicio3.py
@@ -0,0 +1,4 @@
+primeiroNome = input("Digite seu primeiro nome: ")
+salarioAbril = input("Digite seu salario no mes de abril: ")
+mensagem = "O salario de " + primeiroNome + " no mes de abril foi de R$" + salarioAbril + " reais."
+print(mensagem)
